d7ca795ea2fdbe269e9248a208cc931cc587700f
John Waldrep <johnwaldrep@gmail.com>
2015-06-18 19:18:07 -0400

    Add Bookmark admin view

--- urly_bird/bookmark/admin.py
+++ urly_bird/bookmark/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bookmark
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['user', 'url', 'short', 'timestamp', 'title', 'description']
 
 # Register your models here.
+admin.site.register(Bookmark, BookmarkAdmin)
