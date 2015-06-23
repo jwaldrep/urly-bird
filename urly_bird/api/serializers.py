51b127d1f90e05f93b75cfd96b082982ebe2cc20
John Waldrep <johnwaldrep@gmail.com>
2015-06-23 10:51:40 -0400

    Add serializers

---
+++ urly_bird/api/serializers.py
@@ -0,0 +1,8 @@
+from bookmark.models import Bookmark
++from rest_framework import serializers
++
++class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
++    class Meta:
++        model = Bookmark
++        fields = ('id', 'url', 'user', 'short', 'timestamp', 'title', 'description') # FIXME: Okay to name url?, readonly?
+
