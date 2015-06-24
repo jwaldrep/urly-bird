300724330948decc852ddec219903788f65188c9
John Waldrep <johnwaldrep@gmail.com>
2015-06-24 14:01:57 -0400

    Add filtering

--- urly_bird/api/views.py
+++ urly_bird/api/views.py
@@ -2,16 +2,30 @@ from bookmark.models import Bookmark, Click
 from api.serializers import BookmarkSerializer, ClickSerializer
 from rest_framework import viewsets
 from api.permissions import IsOwnerOrReadOnly, OwnsRelatedBookmark
-from rest_framework import viewsets, permissions, generics
+from rest_framework import viewsets, permissions, generics, filters
 from rest_framework.exceptions import PermissionDenied
+import django_filters
+
+
+class BookmarkFilter(django_filters.FilterSet):
+    user = django_filters.CharFilter(name="user", lookup_type="icontains")
+    url = django_filters.CharFilter(name="url", lookup_type="icontains")
+    title = django_filters.CharFilter(name="title", lookup_type="icontains")
+    description = django_filters.CharFilter(name="description", lookup_type="icontains")
+
+    class Meta:
+        model = Bookmark
+        fields = ['user', 'url', 'title', 'description']
 
 
 class BookmarkViewSet(viewsets.ModelViewSet):
+    """Use name and notes GET parameters to filter."""
     queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
-
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = BookmarkFilter
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)  # FIXME: Breaks on anon user
--- urly_bird/urly_bird/settings.py
+++ urly_bird/urly_bird/settings.py
@@ -140,6 +140,6 @@ BOOTSTRAP3 = {
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',), #FIXME: Is this needed?
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
 }
